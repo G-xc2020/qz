5ec4ea2a112096014d9134f7053940bce072e3bd
G-xc2020 <74646831+G-xc2020@users.noreply.github.com>
2020-12-11 20:38:34 +0800

    Add files via upload

---
+++ duiliec.c
@@ -0,0 +1,16 @@
+#include "duilie.h"
+
+int main() {
+	Deque *dq = createDeque();
+	push_back(dq, 80);
+	push_front(dq, 90);
+	push_back(dq, 70);
+	push_front(dq, 60);
+	pop_back(dq);
+
+	printf("队列的元素个数为: %d\n", size(dq));
+	printf("队头元素为: %d\n", front(dq));
+	printf("队尾元素为: %d\n", back(dq));
+	printf("姓名：郭秀辰  学号：45\n");
+}
+
